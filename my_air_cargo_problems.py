5fefca00f069dd9bbf5d7d065d55f5ed107077a4
Joseph Hwang <hwangjk012@hotmail.com>
2017-05-24 14:04:05 -0500

    update

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -69,12 +69,6 @@ class AirCargoProblem(Problem):
             list of Action objects
         """
 
-        # TODO create concrete Action objects based on the domain action schema for: Load, Unload, and Fly
-        # concrete actions definition: specific literal action that does not include variables as with the schema
-        # for example, the action schema 'Load(c, p, a)' can represent the concrete actions 'Load(C1, P1, SFO)'
-        # or 'Load(C2, P2, JFK)'.  The actions for the planning problem must be concrete because the problems in
-        # forward search and Planning Graphs must use Propositional Logic
-
         def load_actions():
             """Create all concrete Load actions and return a list
 
@@ -277,8 +271,6 @@ def air_cargo_p1() -> AirCargoProblem:
 
 
 def air_cargo_p2() -> AirCargoProblem:
-    # TODO implement Problem 2 definition
-
 # - Problem 2 initial state and goal:
 # ```
 # Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL) 
@@ -330,8 +322,6 @@ def air_cargo_p2() -> AirCargoProblem:
 
 
 def air_cargo_p3() -> AirCargoProblem:
-    # TODO implement Problem 3 definition
-
 # - Problem 3 initial state and goal:
 # ```
 # Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL) ∧ At(C4, ORD) 
